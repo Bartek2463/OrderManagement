a352d5d033e54c51d759154a0fc4cb9c925a720a
Bartek2463 <szybiakbartlomiej@gmail.com>
2023-06-22 23:31:06 +0200

    Add LoginDto

--- src/main/java/com/example/ordermanagement/model/user/User.java
+++ src/main/java/com/example/ordermanagement/model/user/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Data
 @NoArgsConstructor
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Table(name="User", uniqueConstraints = {
         @UniqueConstraint(columnNames = {"nameUser"}),
         @UniqueConstraint(columnNames = {"email"})})
+@Accessors(chain = true,fluent = false)
 public class User {
 
     @Id
--- src/main/java/com/example/ordermanagement/model/user/dto/UserLoginDto.java
+++ src/main/java/com/example/ordermanagement/model/user/dto/UserLoginDto.java
@@ -1,4 +1,16 @@
 package com.example.ordermanagement.model.user.dto;
 
+import lombok.*;
+import lombok.experimental.Accessors;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Accessors(chain = true)
 public class UserLoginDto {
+    private Long id;
+    private String username;
+    private String password;
 }
