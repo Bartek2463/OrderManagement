d91aedb677e5095931247f26628697f26d366b3a
Bartek2463 <szybiakbartlomiej@gmail.com>
2023-06-22 21:11:02 +0200

    Application for Order Management

---
+++ src/main/java/com/example/ordermanagement/controller/Order.java
@@ -0,0 +1,17 @@
+package com.example.ordermanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Order {
+
+    @GetMapping("/{order}")
+    public String getOrder(){
+        return "Twice single page";
+    }
+
+}
