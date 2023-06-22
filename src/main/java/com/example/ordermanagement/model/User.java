a7fa3614f1f1208fce1646ab7f4b693376da46cd
Bartek2463 <szybiakbartlomiej@gmail.com>
2023-06-22 22:44:35 +0200

    Crate a client, company owner and admin

---
+++ src/main/java/com/example/ordermanagement/model/User.java
@@ -0,0 +1,14 @@
+package com.example.ordermanagement.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class User {
+
+    private String name;
+    private String Surname;
+
+
+}
