8ef9f4036f59569765321be701ed0c7484df98a9
Bartek2463 <szybiakbartlomiej@gmail.com>
2023-06-23 13:35:03 +0200

    Create Model for User,Order company,Admin application

--- src/main/java/com/example/ordermanagement/model/User.java
+++ src/main/java/com/example/ordermanagement/model/User.java
@@ -1,4 +1,44 @@
 package com.example.ordermanagement.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "User", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"username"}),
+        @UniqueConstraint(columnNames = {"email"})
+})
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "user_name", nullable = false)
+    @NonNull
+    private String userName;
+
+    @Column(name = "password", nullable = false)
+    @NonNull
+    @Size(min = 8, max = 14, message = "Password must have contain at least 8 characters or less 14 characters")
+    private String password;
+    @Column(name = "email", nullable = false)
+    @Email
+    private String email;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+
+
 }
