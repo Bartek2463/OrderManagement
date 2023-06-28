504ca896eb5f7282b5578fb16949f74afa9650b7
Bartek2463 <szybiakbartlomiej@gmail.com>
2023-06-28 13:10:58 +0200

    Create method for delete, update, find users and owners in the application

--- src/main/java/com/example/ordermanagement/service/UserServiceImpl.java
+++ src/main/java/com/example/ordermanagement/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.ordermanagement.service;
 
+import com.example.ordermanagement.model.DTO.UserDetailsDTO;
+import com.example.ordermanagement.model.DTO.UserListDTO;
 import com.example.ordermanagement.model.DTO.UserRegisterDTO;
 import com.example.ordermanagement.model.UserRole;
 import com.example.ordermanagement.model.Users;
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -39,6 +42,40 @@ public class UserServiceImpl implements UserService {
     @Override
     public Optional<Users> searchUserEmail(String email) {
         return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public Optional<Users> serachById(Long ownerId) {
+        return userRepository.findById(ownerId);
+    }
+
+    @Override
+    public List<UserListDTO> getAllUsers() {
+        List<Users> allUsers = userRepository.findAll();
+        return UserListDTO.mapToDto(allUsers);
+    }
+
+    @Override
+    public UserDetailsDTO updateById(UserDetailsDTO detailsDto, Long id) {
+        Users editUsers = serachById(id).orElseThrow();
+
+        editUsers.setUserName(detailsDto.getFirstName());
+        editUsers.setEmail(detailsDto.getEmail());
+        editUsers.setFirstName(detailsDto.getFirstName());
+        editUsers.setLastName(detailsDto.getLastName());
+        detailsDto.setPassword(passwordEncoder.encode(detailsDto.getPassword()));
+        editUsers.setPassword(detailsDto.getPassword());
+
+        System.out.println("editUsers after = " + editUsers);
+        Users saveEditUser = userRepository.save(editUsers);
+        System.out.println(" after saveEditUser = " + saveEditUser);
+
+        return UserDetailsDTO.mapToDto(saveEditUser);
+    }
+
+    @Override
+    public void delete(Long appId) {
+       userRepository.deleteById(appId);
     }
 }
 
