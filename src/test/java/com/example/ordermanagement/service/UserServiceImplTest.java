d87b04a9439dac91111f8ef36dfe365cd913a662
Bartek2463 <szybiakbartlomiej@gmail.com>
2023-09-30 22:28:59 +0200

    Test mock in User Service and UserRepository

--- src/test/java/com/example/ordermanagement/service/UserServiceImplTest.java
+++ src/test/java/com/example/ordermanagement/service/UserServiceImplTest.java
@@ -1,10 +1,19 @@
 package com.example.ordermanagement.service;
 
+import com.example.ordermanagement.model.DTO.UserListDTO;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 class UserServiceImplTest {
 
@@ -33,7 +42,17 @@ class UserServiceImplTest {
     }
 
     @Test
-    void getAllUsers() {
+    void shouldReturnGetAllUsers() {
+        UserService userService = mock(UserService.class);
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
+        given(userService.getAllUsers()).willReturn(Arrays.asList());
+
+
+        List<UserListDTO> allUsers = userServiceImpl.getAllUsers();
+        allUsers.add(0,1)
+
+        assertThat(allUsers,is(null));
+
     }
 
     @Test
