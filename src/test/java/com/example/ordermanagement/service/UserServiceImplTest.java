0905d600627838e04d424a464debb8e3dd0742fc
Bartek2463 <szybiakbartlomiej@gmail.com>
2023-09-30 22:19:36 +0200

    Test mock in User Service and UserRepository

---
+++ src/test/java/com/example/ordermanagement/service/UserServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.example.ordermanagement.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void saveUser() {
+    }
+
+    @Test
+    void searchUserName() {
+    }
+
+    @Test
+    void searchUserEmail() {
+    }
+
+    @Test
+    void serachById() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void updateById() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
