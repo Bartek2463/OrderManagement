d7324284f60144cdc60a1d78606a5a88f1f33650
Bartek2463 <szybiakbartlomiej@gmail.com>
2024-01-07 02:49:38 +0100

    Update method saveJobOrder

--- src/main/java/com/example/ordermanagement/service/JobOrderServiceImpl.java
+++ src/main/java/com/example/ordermanagement/service/JobOrderServiceImpl.java
@@ -1,10 +1,12 @@
 package com.example.ordermanagement.service;
 
+import com.example.ordermanagement.exception.ElementNotFoundException;
 import com.example.ordermanagement.model.order.JobOrder;
 import com.example.ordermanagement.model.order.dto.JobOrderDetailsDTO;
 import com.example.ordermanagement.model.order.dto.JobOrderDetailsDtoUP;
 import com.example.ordermanagement.model.order.dto.JobOrderListDTO;
 import com.example.ordermanagement.repository.JobOrderRepository;
+import com.example.ordermanagement.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,9 +21,12 @@ public class JobOrderServiceImpl implements JobOrderService {
     @Autowired
     private JobOrderRepository jobOrderRepository;
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
+
     @Override
-    public JobOrderDetailsDTO saveJobOrder(JobOrder jobOrder,Long id) {
+    public JobOrderDetailsDTO saveJobOrder(JobOrder jobOrder, Long id) {
+
+        userRepository.findById(id).orElseThrow(() -> new ElementNotFoundException("JobOrder", "ID", id.toString()));
 
         JobOrder save = jobOrderRepository.save(jobOrder);
         return JobOrderDetailsDTO.mapToDto(save);
@@ -29,7 +34,7 @@ public class JobOrderServiceImpl implements JobOrderService {
 
     @Override
     public Optional<JobOrder> searchJobOrderDate(LocalDate localDate) {
-       return jobOrderRepository.findByDateJobOrder(localDate);
+        return jobOrderRepository.findByDateJobOrder(localDate);
     }
 
     @Override
@@ -39,30 +44,30 @@ public class JobOrderServiceImpl implements JobOrderService {
 
     @Override
     public Optional<JobOrder> searchById(Long id) {
-      return   jobOrderRepository.findById(id);
+        return jobOrderRepository.findById(id);
     }
 
     @Override
     public List<JobOrderListDTO> getAllJobOrders() {
         List<JobOrder> allJobs = jobOrderRepository.findAll();
-      return JobOrderListDTO.mapToDto(allJobs);
+        return JobOrderListDTO.mapToDto(allJobs);
     }
 
     @Override
     public JobOrderDetailsDtoUP updateByid(JobOrderDetailsDtoUP jobOrderDetailsDtoUp, Long id) {
         Optional<JobOrder> jobOrder = searchById(id);
-         if (jobOrder.isEmpty()){
-             return null;
-         }
-         jobOrderDetailsDtoUp.setDateJobOrder(jobOrderDetailsDtoUp.getDateJobOrder());
-         jobOrderDetailsDtoUp.setPrice(jobOrderDetailsDtoUp.getPrice());
-         jobOrderDetailsDtoUp.setDescription(jobOrderDetailsDtoUp.getDescription());
+        if (jobOrder.isEmpty()) {
+            return null;
+        }
+        jobOrderDetailsDtoUp.setDateJobOrder(jobOrderDetailsDtoUp.getDateJobOrder());
+        jobOrderDetailsDtoUp.setPrice(jobOrderDetailsDtoUp.getPrice());
+        jobOrderDetailsDtoUp.setDescription(jobOrderDetailsDtoUp.getDescription());
         JobOrder saveEditJobOrder = jobOrderRepository.save(jobOrder.get());
         return JobOrderDetailsDtoUP.mapToDto(saveEditJobOrder);
     }
 
     @Override
     public void delete(Long jobId) {
-     jobOrderRepository.deleteById(jobId);
+        jobOrderRepository.deleteById(jobId);
     }
 }
